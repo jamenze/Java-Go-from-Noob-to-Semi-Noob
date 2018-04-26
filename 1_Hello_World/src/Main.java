a6a336c9c7e813fbdac1c654b6139f0da445befe
Jennifer Menze <jamenze@gmail.com>
2018-04-26 15:42:12 -0400

    Second commit

---
+++ 1_Hello_World/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Print Hello World on new line
+		System.out.println("Hello World");
+		
+		// Print Hello WorldHello World on same line
+		System.out.print("Hello World");
+		System.out.print("Hello World");
+
+	}
+
+}
